9021ed4a6830f10f91ce8d5d7f0d608c027c930d
shivakumar886 <shivukumar.xworkz@gmail.com>
2023-09-06 09:42:29 +0530

    for demo

---
+++ Appliction.java
@@ -0,0 +1,13 @@
+public class Appliction {
+public static void main(String[] args){
+String pendrivename="vbhbx hb";
+String pendrivecolor="black";
+double pendnriveweight=35.22;
+double pendrivespeed=52.23;
+
+System.out.println(pendrivename);
+System.out.println(pendrivecolor);
+System.out.println(pendnriveweight);
+System.out.println(pendrivespeed);
+}
+}
